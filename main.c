4bed9709e4d6eb835dcfab0a1b4aff5501a7b280
Yared S <s.yared25@gmail.com>
2022-06-16 10:54:41 +0300

    Update on main

--- main.c
+++ main.c
@@ -1,54 +1,59 @@
 #include "shell.h"
-/*
- *
- * main - Contains the main functions
- *
- */
+
 int main(int argc __attribute__((unused)), char **argv)
 {
-appData_t *appData = NULL;
-int cLoop;
-void (*func)(appData_t *);
+	appData_t *appData = NULL;
+	int cLoop;
+	void (*func)(appData_t *);
 
-appData = _initData(argv);
-do {
-	signal(SIGINT, _ctrlC);
-	_prompt();
-	_getline(appData);
-	appData->history = _strtow(appData->buffer, COMMAND_SEPARATOR, ESCAPE_SEPARATOR);
-	if (appData->history == NULL)
-	{
-		_freeAppData(appData);
-		free(appData);
-		continue;
-	}
-	for (cLoop = 0; appData->history[cLoop] != NULL; cLoop++)
-	{
-		appData->arguments = _strtow(appData->history[cLoop], SEPARATORS, ESCAPE_SEPARATOR);
-		if (appData->arguments == NULL)
+	appData = _initData(argv);
+
+	do {
+		signal(SIGINT, _ctrlC);
+		_prompt();
+
+		_getline(appData);
+
+		appData->history = _strtow(appData->buffer, COMMAND_SEPARATOR, ESCAPE_SEPARATOR);
+
+		if (appData->history == NULL)
 		{
 			_freeAppData(appData);
-			_freeEnvList(appData->env);
-			appData->env = NULL;
 			free(appData);
-			appData = NULL;
-			break;
+			continue;
 		}
-		appData->commandName = _strdup(appData->arguments[0]);
-		if (appData->commandName != NULL)
+
+		for (cLoop = 0; appData->history[cLoop] != NULL; cLoop++)
 		{
-			func = _getCustomFunction(appData->commandName);
-			if (func != NULL)
-				func(appData);
-			else
-				_execCommand(appData);
+			appData->arguments = _strtow(appData->history[cLoop], SEPARATORS, ESCAPE_SEPARATOR);
+
+			if (appData->arguments == NULL)
+			{
+				_freeAppData(appData);
+				_freeEnvList(appData->env);
+				appData->env = NULL;
+				free(appData);
+				appData = NULL;
+				break;
+			}
+
+			appData->commandName = _strdup(appData->arguments[0]);
+
+			if (appData->commandName != NULL)
+			{
+				func = _getCustomFunction(appData->commandName);
+				if (func != NULL)
+					func(appData);
+				else
+					_execCommand(appData);
+			}
+			_freeCharDoublePointer(appData->arguments);
+			appData->arguments = NULL;
+			free(appData->commandName);
+			appData->commandName = NULL;
 		}
-		_freeCharDoublePointer(appData->arguments);
-		appData->arguments = NULL;
-		free(appData->commandName);
-		appData->commandName = NULL;
-	}
-	_freeAppData(appData);
-} while (1);
+
+		_freeAppData(appData);
+	} while (1);
 	return (EXIT_SUCCESS);
 }
